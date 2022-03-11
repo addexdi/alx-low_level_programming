358b676af78a06189753e31a5e6738fe1e32fe03
Adam Ibrahim <onelordad@gmail.com>
2022-03-11 01:21:52 +0100

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
+#define CONSTANT "\"Programming is like building a multilingual puzzle"
+
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise.
+ */
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle");
-    return (0);
+	puts(CONSTANT);
+	return (0);
 }
