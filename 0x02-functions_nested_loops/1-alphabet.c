539c7b59249212c33ed077ce8c83d65aeb306aae
[Adam Ibrahim] <onelordad@gmail.com>
2022-03-15 23:18:50 +0100

    alphabelt in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+
+/**
+ * main - program that prints the alphabet in lowercase,
+ * followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+
+	putchar('\n');
+	return (0);
+}
