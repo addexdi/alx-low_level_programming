408170134608455501c6a21b8df24b5b7c5e13ea
[Adam Ibrahim] <onelordad@gmail.com>
2022-04-05 01:47:33 +0100

    Recurrsion

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * wildcmp - compares a string and a wild card
+ *
+ * @s1: given string
+ * @s2: wildcard string for comparison
+ *
+ * Return: 1 if strings are identical otherwise return 0
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+		return (0);
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+	return (0);
+}
